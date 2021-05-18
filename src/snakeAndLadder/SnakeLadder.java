1ca2928f806057fa77d8e247e53c6da1ac61c211
Sanjay Pradeep <prasanth.n@yourdost.com>
2021-05-18 21:30:21 +0530

    Sanjay - 'Few Optimization'

--- src/snakeAndLadder/SnakeLadder.java
+++ src/snakeAndLadder/SnakeLadder.java
@@ -8,10 +8,17 @@ import java.util.stream.IntStream;
 class Snake {
     int head;
     int tail;
+    int snakeId;
+    static int primaryId;
 
     Snake(int head, int tail) {
         this.head = head;
         this.tail = tail;
+        this.snakeId = generateId();
+    }
+
+    int generateId() {
+        return ++primaryId;
     }
 }
 
@@ -25,21 +32,52 @@ class Ladder {
     }
 }
 
-class BoardPosition {
+class BoardTile {
     Ladder ladder;
     Snake snake;
 
-    BoardPosition(Ladder ladder) {
+    BoardTile(Ladder ladder) {
         this.ladder = ladder;
     }
 
-    BoardPosition(Snake snake) {
+    BoardTile(Snake snake) {
         this.snake = snake;
     }
 
-    public BoardPosition() {
+    public BoardTile() {
         this.ladder = null;
         this.snake = null;
+    }
+}
+
+class Board {
+    BoardTile[] boardTiles;
+
+    Board(int size) {
+        boardTiles = new BoardTile[size];
+        createBoard(size);
+    }
+
+    void createBoard(int size) {
+        for (int i = 0; i < size; i++) {
+            boardTiles[i] = new BoardTile();
+        }
+    }
+
+    void printBoard() {
+        System.out.println("Print board");
+        for (int i = boardTiles.length - 1; i >= 1; i--) {
+            if (boardTiles[i].snake != null) {
+                System.out.print("S  ");
+            } else if (boardTiles[i].ladder != null) {
+                System.out.print("L  ");
+            } else {
+                System.out.print(i + " ");
+            }
+            if (i % 10 == 1) {
+                System.out.println();
+            }
+        }
     }
 }
 
@@ -59,11 +97,10 @@ class Player {
 }
 
 public class SnakeLadder {
-    static BoardPosition[] board = new BoardPosition[101];
+    static Board board;
 
     static String movedToPosition(Player player, int diceRolled) {
         int playerCurrentPosition = player.playerPosition;
-//        MovePlayerPosition movePlayerPosition = null;
         String moveReason = "";
         int moveToPosition = playerCurrentPosition + diceRolled;
         moveReason = "moved from " + playerCurrentPosition + " to " + moveToPosition;
@@ -71,15 +108,12 @@ public class SnakeLadder {
         if (moveToPosition > 100) {
             moveToPosition = player.playerPosition;
             moveReason = "rolled dice exceeded 100, so loses the turn";
-//            movePlayerPosition = new MovePlayerPosition(moveToPosition, moveReason);
-        } else if (board[moveToPosition].snake != null) {
-            moveToPosition = board[moveToPosition].snake.tail;
+        } else if (board.boardTiles[moveToPosition].snake != null) {
+            moveToPosition = board.boardTiles[moveToPosition].snake.tail;
             moveReason += " but got bitten by the snake and dragged to " + moveToPosition;
-//            movePlayerPosition = new MovePlayerPosition(moveToPosition, moveReason);
-        } else if (board[moveToPosition].ladder != null) {
-            moveToPosition = board[moveToPosition].ladder.end;
-            moveReason += " caught the ladder and rised to " + moveToPosition;
-//            movePlayerPosition = new MovePlayerPosition(moveToPosition, moveReason);
+        } else if (board.boardTiles[moveToPosition].ladder != null) {
+            moveToPosition = board.boardTiles[moveToPosition].ladder.end;
+            moveReason += " caught the ladder and risen to " + moveToPosition;
         }
 
         player.playerPosition = moveToPosition;
@@ -89,15 +123,15 @@ public class SnakeLadder {
     static void startGameStimulation(Player[] player, int[] diceRange) {
         boolean winnerNotDetermined = true;
         int dieRoll;
-        Random rand = new Random();
-        int x = rand.nextInt(10);
-        int playerCount = player.length;
         int playerToRoll = 0;
+        int playerCount = player.length;
         while (winnerNotDetermined) {
             dieRoll = +(int) (Math.random() * ((diceRange[1] - diceRange[0]) + 1)) + diceRange[0];
+
             System.out.print(player[playerToRoll].name + " rolled a " + dieRoll + " and ");
             String movePlayerPosition = movedToPosition(player[playerToRoll], dieRoll);
             System.out.println(movePlayerPosition);
+
             if (player[playerToRoll].playerPosition == 100) {
                 winnerNotDetermined = false;
                 System.out.println(player[playerToRoll].name + " WINS ");
@@ -105,7 +139,7 @@ public class SnakeLadder {
             if (dieRoll != 1 && dieRoll != 6) {
                 playerToRoll++;
             }
-            if (playerToRoll == 6) {
+            if (playerToRoll == playerCount) {
                 playerToRoll = 0;
             }
         }
@@ -115,18 +149,21 @@ public class SnakeLadder {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
+        board = new Board(101);
+        board.printBoard();
+
         int snakes = Integer.parseInt(in.nextLine());
         for (int snakeN : IntStream.range(0, snakes).toArray()) {
             int[] snakePosition = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             Snake snake = new Snake(snakePosition[0], snakePosition[1]);
-            board[snakePosition[0] - 1] = new BoardPosition(snake);
+            board.boardTiles[snakePosition[0] - 1] = new BoardTile(snake);
         }
 
         int ladders = Integer.parseInt(in.nextLine());
         for (int ladderN : IntStream.range(0, ladders).toArray()) {
             int[] ladderPosition = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             Ladder ladder = new Ladder(ladderPosition[0], ladderPosition[1]);
-            board[ladderPosition[0] - 1] = new BoardPosition(ladder);
+            board.boardTiles[ladderPosition[0] - 1] = new BoardTile(ladder);
         }
 
         int players = Integer.parseInt(in.nextLine());
@@ -136,30 +173,8 @@ public class SnakeLadder {
             player[playerNo] = new Player(playerName);
         }
 
-        printBoard();
-
         int[] diceRange = new int[]{1, 6};
         startGameStimulation(player, diceRange);
-    }
-
-    private static void printBoard() {
-        System.out.println("Print board");
-        for (int i = 100; i >= 1; i--) {
-            if (board[i] == null) {
-                System.out.print(i + 1 + " ");
-                board[i] = new BoardPosition();
-            } else if (board[i].snake != null) {
-                System.out.print("S  ");
-            } else if (board[i].ladder != null) {
-                System.out.print("L  ");
-            } else {
-                System.out.print(i + 1 + " ");
-            }
-            if (i % 10 == 0) {
-                System.out.println();
-            }
-        }
-        System.out.println();
     }
 
 }
